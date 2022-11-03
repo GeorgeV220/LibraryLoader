9b07b2252dcfef5e4e1f1d034fadb612100a3453
George V <georgev22.github@gmail.com>
2022-11-04 00:13:46 +0200

    feat(LibraryLoader): LibraryLoader first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,46 @@
+## [1.3.1](https://github.com/GeorgeV220/Hunter/compare/v1.3.0...v1.3.1) (2022-10-28)
+
+
+### Bug Fixes
+
+* **Titles:** Title configurations ([3e68945](https://github.com/GeorgeV220/Hunter/commit/3e6894519c561940df013ad5e7e72c30d8b938d3))
+
+# [1.3.0](https://github.com/GeorgeV220/Hunter/compare/v1.2.1...v1.3.0) (2022-10-28)
+
+
+### Bug Fixes
+
+* **Updater:** Updater URL ([3dde47b](https://github.com/GeorgeV220/Hunter/commit/3dde47b8f3a769de1341a41294e37b9050799338))
+
+
+### Features
+
+* **Holograms:** HologramAPI ProtocolLib support ([0d60ba6](https://github.com/GeorgeV220/Hunter/commit/0d60ba615e6ff815c7c1c815dd6313eee272fabc))
+
+## [1.2.1](https://github.com/GeorgeV220/Hunter/compare/v1.2.0...v1.2.1) (2022-10-26)
+
+
+### Bug Fixes
+
+* **build.gradle:** Change to GeorgeV22 repo for API ([990a398](https://github.com/GeorgeV220/Hunter/commit/990a398bc21a0dfe10f04907c2affce704b819b9))
+
+# [1.2.0](https://github.com/GeorgeV220/Hunter/compare/v1.1.0...v1.2.0) (2022-10-26)
+
+
+### Features
+
+* **publish:** Publish to GeorgeV22 repository ([5e61f32](https://github.com/GeorgeV220/Hunter/commit/5e61f32e819a2808af11d59b8569b373b91790a8))
+
+# [1.1.0](https://github.com/GeorgeV220/Hunter/compare/v1.0.0...v1.1.0) (2022-10-05)
+
+
+### Features
+
+* **Updater:** Added updater check on onEnable ([fdfe418](https://github.com/GeorgeV220/Hunter/commit/fdfe4182632ad2c6a470567bf1352385a9bde3d7))
+
+# 1.0.0 (2022-10-05)
+
+
+### Features
+
+* **Update:** Update all dependencies ([21aa591](https://github.com/GeorgeV220/Hunter/commit/21aa59168aef218829ce49891d262f0c646b24fd))
---
+++ src/main/java/com/georgev22/api/libraryloader/ClassLoaderAccess.java
@@ -0,0 +1,171 @@
+package com.georgev22.api.libraryloader;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Collection;
+
+/**
+ * Provides access to {@link ClassLoader} to add URLs on runtime.
+ *
+ * <p>Edited by <a href="https://github.com/GeorgeV220">GeorgeV22</a> to work with {@link ClassLoader}</p>
+ * <p></p>
+ * Original class: <a href="https://github.com/lucko/helper/blob/master/helper/src/main/java/me/lucko/helper/maven/URLClassLoaderAccess.java">https://github.com/lucko/helper/blob/master/helper/src/main/java/me/lucko/helper/maven/URLClassLoaderAccess.java</a>
+ */
+public class ClassLoaderAccess {
+    private final Collection<URL> unopenedURLs;
+    private final Collection<URL> pathURLs;
+
+    private final ClassLoader classLoader;
+
+    private static volatile Object theUnsafe;
+
+    static {
+        try {
+            synchronized (ClassLoaderAccess.class) {
+                if (theUnsafe == null) {
+                    Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
+                    Field theUnsafeField = unsafeClass.getDeclaredField("theUnsafe");
+                    theUnsafeField.setAccessible(true);
+                    theUnsafe = theUnsafeField.get(null);
+                }
+            }
+        } catch (Exception e) {
+            theUnsafe = null;
+        }
+    }
+
+    private static boolean isUnsafeAvailable() {
+        return theUnsafe != null;
+    }
+
+
+    /**
+     * Creates a {@link ClassLoaderAccess} for the given URLClassLoader.
+     *
+     * @param classLoader the class loader
+     */
+    public ClassLoaderAccess(URLClassLoader classLoader) {
+        this.classLoader = classLoader;
+        Collection<URL> unopenedURLs;
+        Collection<URL> pathURLs;
+        try {
+            Object ucp = fetchField(classLoader.getClass(), classLoader, "ucp");
+            unopenedURLs = (Collection<URL>) fetchField(ucp.getClass(), ucp, "unopenedUrls");
+            pathURLs = (Collection<URL>) fetchField(ucp.getClass(), ucp, "path");
+        } catch (Throwable e) {
+            try {
+                Object ucp = fetchField(classLoader.getClass(), classLoader, "ucp");
+                unopenedURLs = (Collection<URL>) fetchField(ucp.getClass(), ucp, "urls");
+                pathURLs = (Collection<URL>) fetchField(ucp.getClass(), ucp, "path");
+            } catch (Throwable e1) {
+                unopenedURLs = null;
+                pathURLs = null;
+                e.printStackTrace();
+                e1.printStackTrace();
+            }
+        }
+        this.unopenedURLs = unopenedURLs;
+        this.pathURLs = pathURLs;
+    }
+
+    /**
+     * Creates a {@link ClassLoaderAccess} for the given ClassLoader.
+     *
+     * @param classLoader the class loader
+     */
+    public ClassLoaderAccess(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+        Collection<URL> unopenedURLs;
+        Collection<URL> pathURLs;
+        try {
+            Object ucp = fetchField(classLoader.getClass().getSuperclass(), classLoader, "ucp");
+            unopenedURLs = (Collection<URL>) fetchField(ucp.getClass(), ucp, "unopenedUrls");
+            pathURLs = (Collection<URL>) fetchField(ucp.getClass(), ucp, "path");
+        } catch (Throwable e) {
+            try {
+                Object ucp = fetchField(classLoader.getClass().getSuperclass(), classLoader, "ucp");
+                unopenedURLs = (Collection<URL>) fetchField(ucp.getClass(), ucp, "urls");
+                pathURLs = (Collection<URL>) fetchField(ucp.getClass(), ucp, "path");
+            } catch (Throwable e1) {
+                unopenedURLs = null;
+                pathURLs = null;
+                e.printStackTrace();
+                e1.printStackTrace();
+            }
+        }
+        this.unopenedURLs = unopenedURLs;
+        this.pathURLs = pathURLs;
+    }
+
+    /**
+     * Adds the given URL to the class loader.
+     *
+     * @param url the URL to add
+     */
+    public void add(@NotNull URL url) throws RuntimeException {
+        if (this.unopenedURLs.contains(url) || this.pathURLs.contains(url)) {
+            throw new RuntimeException("URL " + url + " already exists in the path");
+        }
+        this.unopenedURLs.add(url);
+        this.pathURLs.add(url);
+    }
+
+    /**
+     * Removes the given URL from the class loader.
+     *
+     * @param url the URL to remove
+     */
+    public void remove(@NotNull URL url) {
+        this.unopenedURLs.remove(url);
+        this.pathURLs.remove(url);
+    }
+
+    public Collection<URL> getPathURLs() {
+        return pathURLs;
+    }
+
+    public Collection<URL> getUnopenedURLs() {
+        return unopenedURLs;
+    }
+
+    private Object fetchField(final Class<?> clazz, final Object object, final String name) throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        if (isUnsafeAvailable()) {
+            Field field = clazz.getDeclaredField(name);
+            long offset = (long) fetchMethodAndInvoke(theUnsafe.getClass(), "objectFieldOffset", theUnsafe, new Object[]{field}, new Class[]{Field.class});
+            return fetchMethodAndInvoke(theUnsafe.getClass(), "getObject", theUnsafe, new Object[]{object, offset}, new Class[]{Object.class, long.class});
+        }
+        try {
+            Field field = clazz.getDeclaredField(name);
+            field.setAccessible(true);
+            return field.get(object);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    private Object fetchMethodAndInvoke(final Class<?> clazz, final String name, Object obj, Object[] arguments, Class<?>[] parameterTypes) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        return fetchMethod(clazz, name, parameterTypes).invoke(obj, arguments);
+    }
+
+    private @NotNull Method fetchMethod(final @NotNull Class<?> clazz, final String name, Class<?>... parameterTypes) throws NoSuchMethodException {
+        Method method = clazz.getDeclaredMethod(name, parameterTypes);
+        if (!isUnsafeAvailable()) {
+            method.setAccessible(true);
+        }
+        return method;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassLoaderAccess{" +
+                "unopenedURLs=" + unopenedURLs +
+                ", pathURLs=" + pathURLs +
+                ", classLoader=" + classLoader.getClass().getPackage().getName() + "." + classLoader.getName() +
+                '}';
+    }
+}
